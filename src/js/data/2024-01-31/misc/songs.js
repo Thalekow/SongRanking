3822e8cce2b16704c16b9c4ef1b55584edcca44a
Thaleko <116354016+Thalekow@users.noreply.github.com>
2024-06-14 19:36:39 +0200

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,8 +1,8 @@
 
 export const songs = [
 	{
-    name: "Psycho-Pass 3 - Q-vism by Who-ya Extended",
-    videoId : "https://nl.catbox.video/ujdgdr.webm",
+    name: "Free! Dive to the Future - Heading to Over by OLDCODEX",
+    videoId : "https://nl.catbox.video/ps9tjx.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -11,8 +11,8 @@ export const songs = [
     }
 },
 	{
-    name: "Jujutsu Kaisen - VIVID VICE by Who-ya Extended",
-    videoId : "https://nl.catbox.video/4lqbch.webm",
+    name: "Asura Cryin' 2 - Alternative by angela",
+    videoId : "https://nl.catbox.video/13zia2.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -21,8 +21,8 @@ export const songs = [
     }
 },
 	{
-    name: "Psycho-Pass 3: First Inspector - Synthetic Sympathy by Who-ya Extended",
-    videoId : "https://nl.catbox.video/udb4eb.webm",
+    name: "Strike the Blood IV - Akatsuki no Kaleido Blood by Kishida Kyoudan &THE Akeboshi Rockets",
+    videoId : "https://nl.catbox.video/dxryf8.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -31,8 +31,8 @@ export const songs = [
     }
 },
 	{
-    name: "Night Head 2041 - Icy Ivy by Who-ya Extended",
-    videoId : "https://nl.catbox.video/2aa84g.webm",
+    name: "Major - Saraba Aoki Omokage by ROAD OF MAJOR",
+    videoId : "https://nl.catbox.video/g2dr6a.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -41,8 +41,8 @@ export const songs = [
     }
 },
 	{
-    name: "Build-Divide: #FFFFFF (Code White) - A Shout Of Triumph by Who-ya Extended",
-    videoId : "https://nl.catbox.video/fli5t6.webm",
+    name: "Senyuu. - Kanariya by GRANRODEO",
+    videoId : "https://nl.catbox.video/jjn7o2.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -51,8 +51,238 @@ export const songs = [
     }
 },
 	{
-    name: "Build-Divide: #FFFFFF (Code White) - Re:Painted by Who-ya Extended",
-    videoId : "https://nl.catbox.video/t0z1qc.webm",
+    name: "Durarara!!×2 Ten - Day you laugh by Toshiyuki Toyonaga",
+    videoId : "https://nl.catbox.video/t45l0f.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Mobile Suit Gundam: Iron-Blooded Orphans - RAGE OF DUST by SPYAIR",
+    videoId : "https://nl.catbox.video/t3hyjj.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Bungo Stray Dogs - Tetsu no Ori by GRANRODEO",
+    videoId : "https://nl.catbox.video/lqgdx8.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Grisaia no Rakuen - Setsuna no Kajitsu by Maon Kurosaki",
+    videoId : "https://nl.catbox.video/h46rp1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Mobile Suit Gundam Seed Destiny - vestige by T.M.Revolution",
+    videoId : "https://nl.catbox.video/1mo11y.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Mobile Suit Gundam: Iron-Blooded Orphans - Fighter by KANA-BOON",
+    videoId : "https://nl.catbox.video/2pnt6v.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Sound! Euphonium 2 - Soundscape by TRUE",
+    videoId : "https://nl.catbox.video/94yyyi.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "K: Seven Stories - SURVIVE! by angela",
+    videoId : "https://nl.catbox.video/nbtqxn.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Magi: The Labyrinth of Magic - Matataku Hoshi no Shita De by Porno Graffitti",
+    videoId : "https://nl.catbox.video/u3ku86.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Strike the Blood II - Blood on the EDGE by Kishida Kyoudan &THE Akeboshi Rockets",
+    videoId : "https://nl.catbox.video/094796.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Mobile Suit Gundam: Iron-Blooded Orphans - Special Edition - Blaze by MAN WITH A MISSION",
+    videoId : "https://nl.catbox.video/l8qdf6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Fafner The Beyond - Sakebe by angela",
+    videoId : "https://nl.catbox.video/ohowg8.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Fullmetal Alchemist: Brotherhood - Rain by SID",
+    videoId : "https://nl.catbox.video/lasmre.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Fafner in the Azure - Shangri-la by angela",
+    videoId : "https://nl.catbox.video/5j32d5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Blue Exorcist - IN MY WORLD by ROOKiEZ is PUNK'D",
+    videoId : "https://nl.catbox.video/l0yogp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Macross Delta - Zettai Reido θ Novatic by Walküre",
+    videoId : "https://nl.catbox.video/p91l58.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "City Hunter 3 - RUNNING TO HORIZON by Tetsuya Komuro",
+    videoId : "https://nl.catbox.video/04xjew.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Naruto Shippuuden - Tsuki no Ookisa by Nogizaka46",
+    videoId : "https://nl.catbox.video/iq60um.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Food Wars! Shokugeki no Soma - Rising Rainbow by Misokkasu",
+    videoId : "https://nl.catbox.video/1zpjt7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Soul Eater - PAPERMOON by Tommy heavenly⁶",
+    videoId : "https://nl.catbox.video/s1lotg.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Bleach - Ichirin no Hana by HIGH and MIGHTY COLOR",
+    videoId : "https://nl.catbox.video/65fk2f.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Aikatsu Stars! - Star Jet! by Sena・Rie・Miki・Kana from AIKATSU☆STARS!",
+    videoId : "https://nl.catbox.video/0g5eup.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "Hai to Gensou no Grimgar - Knew day by (K)NoW_NAME:Ayaka Tachibana",
+    videoId : "https://nl.catbox.video/jbykqo.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "JoJo no Kimyou na Bouken: Diamond wa Kudakenai - chase by batta",
+    videoId : "https://nl.catbox.video/3zv11w.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -60,8 +290,8 @@ export const songs = [
         ]
     }
 },{
-    name: "Hametsu no Oukoku - Prayer by Who-ya Extended",
-    videoId : "https://nl.catbox.video/wpj3r6.webm",
+    name: "Shirobako - Alice in Blue by Rita",
+    videoId : "https://nl.catbox.video/rjzutd.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
