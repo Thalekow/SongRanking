363be70ea4eb33411549cb21adbee341bc88182a
Thaleko <116354016+Thalekow@users.noreply.github.com>
2024-06-28 21:30:47 +0200

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,7 +1,7 @@
 
 export const songs = [
 	{
-    name: "Ousama Ranking - "Hadaka no Yuusha" By Vaundy",
+    name: "Ousama Ranking - Hadaka no Yuusha By Vaundy",
     videoId : "https://nl.catbox.video/4j6rpg.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -11,7 +11,7 @@ export const songs = [
     }
 },
 	{
-    name: "Listeners - "Into the blue's" By ACCAMER",
+    name: "Listeners - Into the blue's By ACCAMER",
     videoId : "https://nl.catbox.video/5e4sne.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -21,7 +21,7 @@ export const songs = [
     }
 },
 	{
-    name: "Anohana : The Flower we saw that day - "Aoi Shiori" By Galileo Galilei",
+    name: "Anohana : The Flower we saw that day - Aoi Shiori By Galileo Galilei",
     videoId : "https://nl.catbox.video/vkay2g.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -31,7 +31,7 @@ export const songs = [
     }
 },
 	{
-    name: "Mushoku Tensei : Jobless Reincarnation - "spiral" By LONGMAN",
+    name: "Mushoku Tensei : Jobless Reincarnation - spiral By LONGMAN",
     videoId : "https://nl.catbox.video/l9yn0w.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -41,7 +41,7 @@ export const songs = [
     }
 },
 	{
-    name: "Dragonball Super - "Chōzetsu ☆ Dynamic!" By Kazuya Yoshii",
+    name: "Dragonball Super - Chōzetsu ☆ Dynamic! By Kazuya Yoshii",
     videoId : "https://nl.catbox.video/v9acyq.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -51,7 +51,7 @@ export const songs = [
     }
 },
 	{
-    name: "Kabaneri Of The Iron Fortress - "KABANERI OF THE IRON FORTRESS" By EGOIST",
+    name: "Kabaneri Of The Iron Fortress - KABANERI OF THE IRON FORTRESS By EGOIST",
     videoId : "https://nl.catbox.video/vn0gx7.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -61,7 +61,7 @@ export const songs = [
     }
 },
 	{
-    name: "Sarazanmai - "Massara" By KANA-BOON",
+    name: "Sarazanmai - Massara By KANA-BOON",
     videoId : "https://nl.catbox.video/360nwx.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -71,7 +71,7 @@ export const songs = [
     }
 },
 	{
-    name: "Kidou Senshi Gundam Seed - "INVOKE" By T.M.Revolution",
+    name: "Kidou Senshi Gundam Seed - INVOKE By T.M.Revolution",
     videoId : "https://nl.catbox.video/2sb46r.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -81,7 +81,7 @@ export const songs = [
     }
 },
 	{
-    name: "Ginga Eiyuu Densetsu: Die Neue These - "Binary star"  By SawanoHiroyuki[nZk]:Uru",
+    name: "Ginga Eiyuu Densetsu: Die Neue These - Binary star  By SawanoHiroyuki[nZk]:Uru",
     videoId : "https://nl.catbox.video/o2pwjj.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -91,7 +91,7 @@ export const songs = [
     }
 },
 	{
-    name: "Space Patrol Luluco - "CRY Max Do Heijitsu" By Fujirokkyu (Kari)",
+    name: "Space Patrol Luluco - CRY Max Do Heijitsu By Fujirokkyu (Kari)",
     videoId : "https://nl.catbox.video/zfb1v0.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -101,7 +101,7 @@ export const songs = [
     }
 },
 	{
-    name: "Kusuriya no Hitorigoto - "Hana ni Natte" By Ryokuoushoku Shakai",
+    name: "Kusuriya no Hitorigoto - Hana ni Natte By Ryokuoushoku Shakai",
     videoId : "https://nl.catbox.video/7ofd1i.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -111,7 +111,7 @@ export const songs = [
     }
 },
 	{
-    name: "Kakushigoto - "Chiisana Hibi" By flumpool",
+    name: "Kakushigoto - Chiisana Hibi By flumpool",
     videoId : "https://nl.catbox.video/c41myn.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -121,7 +121,7 @@ export const songs = [
     }
 },
 	{
-    name: "Free! Eternal Summer - "Dried up Youthful Fame" By OLDCODEX",
+    name: "Free! Eternal Summer - Dried up Youthful Fame By OLDCODEX",
     videoId : "https://nl.catbox.video/bbfh5y.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -131,7 +131,7 @@ export const songs = [
     }
 },
 	{
-    name: "Highschool Of The Dead - "HIGHSCHOOL OF THE DEAD" By Kishida Kyoudan &THE Akeboshi Rockets",
+    name: "Highschool Of The Dead - HIGHSCHOOL OF THE DEAD By Kishida Kyoudan &THE Akeboshi Rockets",
     videoId : "https://nl.catbox.video/63q21z.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -141,7 +141,7 @@ export const songs = [
     }
 },
 	{
-    name: "Monogatari Series Second Season - "Kogarashi sentiment" By  Chiwa Saito, Shinichiro Miki",
+    name: "Monogatari Series Second Season - Kogarashi sentiment By  Chiwa Saito, Shinichiro Miki",
     videoId : "https://nl.catbox.video/freyju.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -151,7 +151,7 @@ export const songs = [
     }
 },
 	{
-    name: "Xian Wang De Ri Chang Sheng Huo - "Xian" by Wei Bo En",
+    name: "Xian Wang De Ri Chang Sheng Huo - Xian by Wei Bo En",
     videoId : "https://www.youtube.com/watch?v=m7AGRVvBFxI",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -161,7 +161,7 @@ export const songs = [
     }
 },
 	{
-    name: "Nichijou - "Hyadain no Joujou Yuujou" By Hyadain",
+    name: "Nichijou - Hyadain no Joujou Yuujou By Hyadain",
     videoId : "https://files.catbox.moe/7t0161.mp4",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -171,7 +171,7 @@ export const songs = [
     }
 },
 	{
-    name: "Fairy Tail - "Believe in myself" By EDGE of LIFE",
+    name: "Fairy Tail - Believe in myself By EDGE of LIFE",
     videoId : "https://nl.catbox.video/858pri.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -181,7 +181,7 @@ export const songs = [
     }
 },
 	{
-    name: "JoJo no Kimyou na Bouken: Diamond wa Kudakenai - "chase" By batta",
+    name: "JoJo no Kimyou na Bouken: Diamond wa Kudakenai - chase By batta",
     videoId : "https://nl.catbox.video/3zv11w.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -191,7 +191,7 @@ export const songs = [
     }
 },
 	{
-    name: "Dorororo - "Kaen" By Ziyoou-vachi",
+    name: "Dorororo - Kaen By Ziyoou-vachi",
     videoId : "https://nl.catbox.video/nguams.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -201,7 +201,7 @@ export const songs = [
     }
 },
 	{
-    name: "Blood-C - "SPIRAL" By DUSTZ",
+    name: "Blood-C - SPIRAL By DUSTZ",
     videoId : "https://nl.catbox.video/bgm307.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -211,7 +211,7 @@ export const songs = [
     }
 },
 	{
-    name: "Bishounen Tanteidan - "Shake & Shake" By sumika",
+    name: "Bishounen Tanteidan - Shake & Shake By sumika",
     videoId : "https://nl.catbox.video/86qja3.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -221,7 +221,7 @@ export const songs = [
     }
 },
 	{
-    name: "Build-Divide: #000000 (Code Black) - "Bang!" By EGOIST",
+    name: "Build-Divide: #000000 (Code Black) - Bang! By EGOIST",
     videoId : "https://nl.catbox.video/3y0ma2.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -231,7 +231,7 @@ export const songs = [
     }
 },
 	{
-    name: "86 EIGHTY-SIX - "Kyoukaisen" By amazarashi",
+    name: "86 EIGHTY-SIX - Kyoukaisen By amazarashi",
     videoId : "https://nl.catbox.video/c43ltw.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -241,7 +241,7 @@ export const songs = [
     }
 },
 	{
-    name: "Inazuma Eleven: Ares no Tenbin - "Teppen e Dash!" By pugcat's",
+    name: "Inazuma Eleven: Ares no Tenbin - Teppen e Dash! By pugcat's",
     videoId : "https://nl.catbox.video/fnyrfc.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -251,7 +251,7 @@ export const songs = [
     }
 },
 	{
-    name: "Call Of The Night - "Daten" By Creepy Nuts",
+    name: "Call Of The Night - Daten By Creepy Nuts",
     videoId : "https://nl.catbox.video/2p06fq.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -261,7 +261,7 @@ export const songs = [
     }
 },
 	{
-    name: "Initial D : Fourth Stage - "DOGFIGHT" By move",
+    name: "Initial D : Fourth Stage - DOGFIGHT By move",
     videoId : "https://nl.catbox.video/3yurmo.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -271,7 +271,7 @@ export const songs = [
     }
 },
 	{
-    name: "Owari No Seraph - "X.U." By SawanoHiroyuki[nZk]:Gemie",
+    name: "Owari No Seraph - X.U. By SawanoHiroyuki[nZk]:Gemie",
     videoId : "https://nl.catbox.video/p1wbgj.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -281,7 +281,7 @@ export const songs = [
     }
 },
 	{
-    name: "One piece - "Bon Voyage" By BON-BON BLANCO",
+    name: "One piece - Bon Voyage By BON-BON BLANCO",
     videoId : "https://nl.catbox.video/m2wa5w.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -290,7 +290,7 @@ export const songs = [
         ]
     }
 },{
-    name: "Mahoutsukai no yome - "Here" By JUNNA",
+    name: "Mahoutsukai no yome - Here By JUNNA",
     videoId : "https://nl.catbox.video/tpp75d.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
