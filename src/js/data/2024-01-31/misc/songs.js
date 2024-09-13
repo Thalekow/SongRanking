8552b8296a80f5b43c3c8a7b084d39aaa2f0c699
Thaleko <116354016+Thalekow@users.noreply.github.com>
2024-09-13 17:43:57 +0200

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,8 +1,8 @@
 
 export const songs = [
 	{
-    name: "Selector Spread WIXOSS - world's end, girl's rondo by Kanon Wakeshima",
-    videoId : "https://nl.catbox.video/tkqcsk.webm",
+    name: "Code Geass: Hangyaku no Lelouch R2 - Face2 by Lozareena",
+    videoId : "https://nl.catbox.video/1tc7hx.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -11,8 +11,8 @@ export const songs = [
     }
 },
 	{
-    name: "3-gatsu no Lion 2nd Season - Flag o Tatero by YUKI",
-    videoId : "https://nl.catbox.video/zm1bub.webm",
+    name: "Youkai Apartment no Yuuga na Nichijou - Good Night Mare by Lozareena",
+    videoId : "https://nl.catbox.video/n1f8il.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -21,8 +21,8 @@ export const songs = [
     }
 },
 	{
-    name: "Star Driver: Kagayaki no Takuto - SHINING☆STAR by 9nine",
-    videoId : "https://nl.catbox.video/nx53k7.webm",
+    name: "Youkai Apartment no Yuuga na Nichijou - Neiro by Lozareena",
+    videoId : "https://nl.catbox.video/jp0gsp.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -31,8 +31,8 @@ export const songs = [
     }
 },
 	{
-    name: "Kidou Senshi Gundam 00 - Ash Like Snow by the brilliant green",
-    videoId : "https://nl.catbox.video/vhku1l.webm",
+    name: "Karakuri Circus - Over me by Lozareena",
+    videoId : "https://nl.catbox.video/p1b3tl.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -41,8 +41,8 @@ export const songs = [
     }
 },
 	{
-    name: "Flip Flappers - Serendipity by ZAQ",
-    videoId : "https://nl.catbox.video/qr7dp1.webm",
+    name: "Karakuri Circus - Marionette by Lozareena",
+    videoId : "https://nl.catbox.video/xxvcwo.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -51,8 +51,8 @@ export const songs = [
     }
 },
 	{
-    name: "Re:␣Hamatora - Sen no Tsubasa by livetune adding Takuro Sugawara (from 9mm Parabellum Bullet)",
-    videoId : "https://nl.catbox.video/75i89h.webm",
+    name: "Kabukichou Sherlock - Hyakuoku Kounen by Lozareena",
+    videoId : "https://nl.catbox.video/cyzrv0.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -61,228 +61,8 @@ export const songs = [
     }
 },
 	{
-    name: "Gakusen Toshi Asterisk 2nd Season - The Asterisk War by Shiena Nishizawa",
-    videoId : "https://nl.catbox.video/taknfj.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Sakurada Reset - Dakara Boku wa Boku o Tebanasu by WEAVER",
-    videoId : "https://nl.catbox.video/46kvi2.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Yuuki Yuuna wa Yuusha de Aru: Yuusha no Shou - Hanakotoba by Sanshuu Chuugaku Yuusha-bu",
-    videoId : "https://nl.catbox.video/jj1fr3.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Kuroshitsuji: Book of Circus - ENAMEL by SID",
-    videoId : "https://nl.catbox.video/czw10c.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Fate/Zero 2nd Season - to the beginning by Kalafina",
-    videoId : "https://nl.catbox.video/k6enn9.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Kidou Senshi Gundam 00 - DAYBREAK'S BELL by L'Arc~en~Ciel",
-    videoId : "https://nl.catbox.video/sidmjw.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Soul Eater - PAPERMOON by Tommy heavenly⁶",
-    videoId : "https://nl.catbox.video/s1lotg.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Fairy Tail - Fairy Tail ~Yakusoku no Hi~ by Chihiro Yonekura",
-    videoId : "https://nl.catbox.video/9fkoug.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Gintama. Shirogane no Tamashii-hen - Kouhan-sen - I Wanna Be... by SPYAIR",
-    videoId : "https://nl.catbox.video/prf893.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Karakai Jouzu no Takagi-san 2 - Zero Centimeter by Yuiko Ohara",
-    videoId : "https://nl.catbox.video/yxafri.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Concrete Revolutio: Choujin Gensou - The Last Song - Wareru Doukoku by ZAQ",
-    videoId : "https://nl.catbox.video/zgeqc4.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Aikatsu Stars! - MUSIC of DREAM!!! by Sena・Rie・Miki・Kana from AIKATSU☆STARS!",
-    videoId : "https://nl.catbox.video/a4ll8y.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Meitantei Conan - Countdown by NormCore",
-    videoId : "https://nl.catbox.video/syfoc3.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Zoids Wild - Kettou by PENGUIN RESEARCH",
-    videoId : "https://nl.catbox.video/k8vob8.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Gundam Build Fighters - wimp by BACK-ON & Lil' Fang (from FAKY)",
-    videoId : "https://nl.catbox.video/lfe6nf.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Code Geass: Hangyaku no Lelouch - Hitomi no Tsubasa by access",
-    videoId : "https://nl.catbox.video/6rb9aq.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Ookami Kakushi - Toki no Mukou Maboroshi no Sora by FictionJunction",
-    videoId : "https://nl.catbox.video/yczo58.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Koukaku Kidoutai: Stand Alone Complex - inner universe by Origa",
-    videoId : "https://nl.catbox.video/zyqioh.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Chain Chronicle: Haecceitas no Hikari - MY LIBERATION by nano",
-    videoId : "https://nl.catbox.video/cc7i46.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Rokka no Yuusha - Black Swallowtail by UROBOROS",
-    videoId : "https://nl.catbox.video/8u03oq.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "D.Gray-man - Doubt & Trust by access",
-    videoId : "https://nl.catbox.video/qtytjj.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Kubikiri Cycle: Ao-iro Savant to Zaregoto Tsukai - Cobalt World by Sangatsu no Phantasia",
-    videoId : "https://nl.catbox.video/e0s5w5.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts:{
-        singer:[
-        "OP"
-        ]
-    }
-},
-	{
-    name: "Magi: The Labyrinth of Magic - Matataku Hoshi no Shita de by Porno Graffitti",
-    videoId : "https://nl.catbox.video/u3ku86.webm",
+    name: "Eiga Entotsu Machi no Poupelle - Entotsu Machi no Poupelle by Lozareena",
+    videoId : "https://nl.catbox.video/3aqxzf.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -290,8 +70,8 @@ export const songs = [
         ]
     }
 },{
-    name: "Sacred Seven - stone cold by FictionJunction",
-    videoId : "https://nl.catbox.video/pc9tfe.webm",
+    name: "Edens Zero - my star by Lozareena",
+    videoId : "https://nl.catbox.video/nwf740.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
